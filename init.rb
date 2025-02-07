972ac26611e612e9f5ad5d3812042385ea05f0c2
André Cristen <30880513+andrecristen@users.noreply.github.com>
2025-02-06 23:49:17 -0300

    view config save

--- init.rb
+++ init.rb
@@ -1,12 +1,9 @@
 Redmine::Plugin.register :multiple_tasks_opener do
-  name 'Multiple Tasks Opener plugin'
+  name 'Multiple Tasks Opener'
   author 'André Cristen'
-  description 'Realiza aberturas multiplas de tarefas'
+  description 'Plugin para abrir múltiplas tarefas no Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/andrecristen/multiple_tasks_opener'
   author_url 'https://github.com/andrecristen'
-  settings default: {}, partial: 'multiple_tasks_opener/settings'
-  project_module :multiple_tasks_opener do
-    permission :manage_multiple_tasks_opener, { multiple_tasks_opener: [:settings, :save_settings] }, public: true
-  end
+  settings default: { 'open_issues' => {} }, partial: 'settings/multiple_tasks_opener_settings'
 end
