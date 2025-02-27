355711374232d743204572179dcb66e292059bbd
André Cristen <30880513+andrecristen@users.noreply.github.com>
2025-02-26 23:57:43 -0300

    repositioned action main

--- lib/multiple_tasks_opener/hooks.rb
+++ lib/multiple_tasks_opener/hooks.rb
@@ -1,6 +1,5 @@
 module MultipleTasksOpener
   class Hooks < Redmine::Hook::ViewListener
-    # Adiciona o botão "Adicionar Sub Tarefas" na tela da issue
-    render_on :view_issues_show_details_bottom, partial: 'issues/add_subtasks_button'
+    render_on :view_issues_show_description_bottom, partial: 'issues/add_subtasks_button'
   end
 end
